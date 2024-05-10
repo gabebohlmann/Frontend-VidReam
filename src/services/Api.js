341a039d4641bed4311d23759d06b79438f783a4
Gabe Bohlmann <gabebohlmann@gmail.com>
2024-05-09 19:50:32 -0500

    Add log for debugging

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 export default () => {
   const axiosInstance = axios.create({
     baseURL: `${process.env.VUE_APP_URL}/api/v1`
-  })
 
+  })
+  console.log('baseURL:' + baseURL)
   const token = localStorage.getItem('token')
   if (token) {
     axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`
